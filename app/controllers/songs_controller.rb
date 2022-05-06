faf80d1a68c4eb89a21cd190b8e5a149a1189fec
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-05-06 12:04:49 -0700

    adds if statement for search params

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,35 @@
 class SongsController < ApplicationController
 
   def index
-    songwriter = Songwriter.find_by(composer: params[:composer]) 
+    # songwriter = Songwriter.find_by(composer: params[:composer]) 
     # songwriters = Songwriter.all
     # p songwriters
     show = Show.find_by(title: params[:title])
-    p songwriter
-    p show
-    songs1 = Song.where(songwriter_id: songwriter.id)
-    songs2 = Song.where(show_id: show.id)
-    # songs = Song.all
-    songs = (songs1 + songs2).uniq
-    render json: songs.as_json
+    
+    composer = params[:composer].split(",")
+    # p composer.length
+    
+    # p params[:title]
+    
+    if composer.length > 1
+      songwriter1 = Songwriter.find_by(composer: composer[0]) 
+      songwriter2 = Songwriter.find_by(composer: composer[1]) 
+    else
+      songwriter = Songwriter.find_by(composer: composer) 
+    end
+
+    if songwriter != nil && show != nil
+      songwriter = Song.where(songwriter_id: songwriter.id)
+      show = Song.where(show_id: show.id)
+      songs = (songwriter + show).uniq
+      render json: songs.as_json
+    elsif songwriter == nil && show != nil
+      show = Song.where(show_id: show.id)
+      rendder json: show.as_json
+    elsif show == nil && songwriter != nil
+      songwriter = Song.where(songwriter_id: songwriter.id)
+      render json: songwriter.as_json
+    end
   end
 
   
