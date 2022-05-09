fd39d8adea2260d27756136215011b239d7385c3
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-05-09 15:57:38 -0700

    adds genres controller

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,8 @@
+class GenresController < ApplicationController
+
+  def index
+    genres = Genre.all
+    render json: genres.as_json
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get "/songwriters" => "songwriters#index"
 
   get "/shows" => "shows#index"
+
+  get "/genres" => "genres#index"
 end
