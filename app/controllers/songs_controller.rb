700630d2b8a128cc495cd1d4e6f05d6fb8453560
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-05-09 17:14:28 -0700

    adds tempos to SQL search

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,11 +5,13 @@ class SongsController < ApplicationController
     composers = params[:composer].split(",")
     shows = params[:show].split(",")
     genres = params[:genre].split(",")
+    tempos = params[:tempo].split(",")
     sql = <<-SQL
       SELECT songs.* FROM songs
       JOIN songwriters on songs.songwriter_id = songwriters.id
       JOIN shows on songs.show_id = shows.id
       JOIN genres on songs.genre_id = genres.id
+      JOIN tempos on songs.tempo_id = tempos.id
       WHERE  songwriters.composer = '#{composers[0]}'
       OR  songwriters.composer = '#{composers[1]}'
       OR  songwriters.composer = '#{composers[2]}'
@@ -18,7 +20,10 @@ class SongsController < ApplicationController
       OR shows.title = '#{shows[2]}'
       OR genres.name = '#{genres[0]}'
       OR genres.name = '#{genres[1]}'
-      OR genres.name = '#{genres[2]}';
+      OR genres.name = '#{genres[2]}'
+      OR tempos.name = '#{tempos[0]}'
+      OR tempos.name = '#{tempos[1]}'
+      OR tempos.name = '#{tempos[2]}';
     SQL
     
     songs = ActiveRecord::Base.connection.select_all(sql)
