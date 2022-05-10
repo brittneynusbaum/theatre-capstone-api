f8110b5fcf5442461c246dbdd04a0bfc814759ca
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-05-10 11:20:26 -0700

    adds dynamic create action for saved_songs

--- app/controllers/saved_songs_controller.rb
+++ app/controllers/saved_songs_controller.rb
@@ -2,8 +2,8 @@ class SavedSongsController < ApplicationController
 
   def create
     audition_song = SavedSong.new(
-      song_id: 4,
-      user_id: 1
+      song_id: params[:song_id],
+      user_id: params[:user_id]
     )
     audition_song.save
     render json: audition_song.as_json
